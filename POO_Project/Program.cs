f9f2f685c0539231634a86d342054427ef6fe7d3
Robin Lamotte <17051@ecam.be>
2020-12-27 23:24:13 +0100

    implentation de class test

--- POO_Project/Program.cs
+++ POO_Project/Program.cs
@@ -7,9 +7,10 @@ namespace POO_Project
 
         static void Main(string[] args)
         {
+            //tout ce qui était ici je l'ai coupé collé dans le fichier classTest_Damien
+            //Ca fait pareil en lancant le programme mais comme ca on sépare nos test
             classTest_Damien test_Damien = new classTest_Damien();
-            //classTest_Robin test_Robin = new classTest_Robin();
-            
+            classTest_Robin test_Robin = new classTest_Robin();
         }
     }
 }
